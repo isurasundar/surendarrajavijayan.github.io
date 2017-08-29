70724aa701792110ea266e84c06615ef7f5bb51d
Surendar Rajavijayan Sundar <surendarrajavijayan@outlook.com>
2017-08-29 11:17:27 -0500

    Update sendemail.php

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'surendarrajavijayan@outlook.com';  // put your email here
+    $to         = 'contact@surasundar.com';  // put your email here
 
     $email_template = 'simple.html';
 
